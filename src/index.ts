c41553dc49ff2409b8e41d9ecfa86657fd9331e3
Sam Hoidal <samhoidal@Sams-MacBook-Pro.local>
2024-10-25 07:31:35 -0600

    add explicit shutdown on sigint and sigterm

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,19 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const shutdown = (signal: string) => {
+  console.log(
+    `Received ${signal}. Shutting down server running on on port http://localhost:${PORT}`
+  );
+  server.close(() => {
+    console.log("Server closed.");
+    process.exit(0);
+  });
+};
+
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+process.on("SIGINT", () => shutdown("SIGINT"));
